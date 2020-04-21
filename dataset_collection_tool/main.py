176d6f29b5206b93960a0e06f5acd050957c873e
vasiliy <ecat3@yandex.ru>
2020-04-21 20:55:45 +0300

    add inference mode scaffold to dataset collection tool

--- dataset_collection_tool/main.py
+++ dataset_collection_tool/main.py
@@ -5,24 +5,24 @@ import tkinter as tk
 import cv2
 
 
-def gen_xy(master):
-    x = random.uniform(0, master.winfo_screenwidth() - 150)
-    y = random.uniform(0, master.winfo_screenheight() - 150)
-    return (x, y)
-
-
 class FullScreenApp:
-    def __init__(self, master, annotation_file, **kwargs):
-        self.annotation_file = annotation_file
+    def __init__(self, master, args):
+        self.annotation_file = args.annotation_file
+        self.inference_mode = args.inference_mode
+        self.cap = cv2.VideoCapture(0)
         self.master = master
         self._geom = '200x200+0+0'
-        self.target_x, self.target_y = gen_xy(master)
         self.look_at_me = tk.Label(master, text="Look\nat me!", bg="orange", font=("Arial Bold", 50))
-        self.look_at_me.place(x=self.target_x, y=self.target_y)
+
         pad = 3
         master.geometry("{0}x{1}+0+0".format(master.winfo_screenwidth() - pad, master.winfo_screenheight() - pad))
         master.bind('<Escape>', self.toggle_geom)
-        master.bind('<space>', self.iteration)
+
+        if self.inference_mode:
+            master.bind('<space>', self.inference_iteration)
+        else:
+            self.random_move()
+            master.bind('<space>', self.random_iteration)
 
     def toggle_geom(self, event):
         geom = self.master.winfo_geometry()
@@ -30,23 +30,36 @@ class FullScreenApp:
         self.master.geometry(self._geom)
         self._geom = geom
 
-    def move_target(self):
-        master = self.master
-        self.target_x, self.target_y = gen_xy(master)
-        self.look_at_me.place(x=self.target_x, y=self.target_y, width=150, height=150)
-
-    def iteration(self, event):
-        global cap
+    def random_iteration(self, event):
         timestamp = time.time_ns()
-        _, frame = cap.read()
+        _, frame = self.cap.read()
         cv2.imwrite("img-{}.png".format(timestamp), frame)
+
         with open(self.annotation_file, "a") as f:
             f.write("{}\t{}\t{}\n".format(
                 timestamp,
                 self.target_x - self.master.winfo_screenwidth() / 2 + 75,
                 self.target_y - self.master.winfo_screenheight() / 2 + 75)
             )
-        self.move_target()
+
+        self.random_move()
+
+    def inference_iteration(self, event):
+        _, frame = self.cap.read()
+        self.infer_and_move(frame)
+
+    def random_move(self):
+        master = self.master
+        x = random.uniform(0, master.winfo_screenwidth() - 150)
+        y = random.uniform(0, master.winfo_screenheight() - 150)
+
+        self.target_x, self.target_y = x, y
+        self.look_at_me.place(x=self.target_x, y=self.target_y, width=150, height=150)
+
+    def infer_and_move(self, frame):
+        self.random_move()  # TODO add inference here
+        # move the label by calling the following:
+        # self.look_at_me.place(x=self.target_x, y=self.target_y, width=150, height=150)
 
 
 def get_args():
@@ -55,20 +68,21 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    output_params = parser.add_argument_group('Output')
-    output_params.add_argument('--annotation-file',
-                               type=str,
-                               default="annotations.tsv",
-                               metavar='FILENAME',
-                               help='File to append annotations to')
-
+    parser.add_argument('--annotation-file',
+                        type=str,
+                        default="annotations.tsv",
+                        metavar='FILENAME',
+                        help='File to append annotations to')
+    parser.add_argument('--inference-mode',
+                        action='store_true',
+                        help='enables inference mode')
     return parser.parse_args()
 
-cap = cv2.VideoCapture(0)
+
 def main():
     args = get_args()
     root = tk.Tk()
-    app = FullScreenApp(root, args.annotation_file)
+    app = FullScreenApp(root, args)
     root.mainloop()
 
 
